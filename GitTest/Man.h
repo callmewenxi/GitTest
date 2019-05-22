7b2631d6c262ec3c7052f6c393bd6a69f641d5b7
callmewenxi <sky0112020179@gmail.com>
2019-05-23 00:34:10 +0800

    add Man class

---
+++ GitTest/Man.h
@@ -0,0 +1,17 @@
+//
+//  Man.h
+//  GitTest
+//
+//  Created by 钟博文 on 2019/5/23.
+//  Copyright © 2019 钟博文. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Man : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
