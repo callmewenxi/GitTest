ccf2da16c03e52b6e5bb835c16c2867d5f9cf1fb
callmewenxi <sky0112020179@gmail.com>
2019-05-23 00:07:46 +0800

    git init

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by 钟博文 on 2019/5/23.
+//  Copyright © 2019 钟博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by 钟博文 on 2019/5/23.
+//  Copyright © 2019 钟博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
